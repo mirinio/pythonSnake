808fbc2fa4d546ab0a8e526782e53e3b566a618d
Amir Awak <amir.kian@awak.ch>
2021-06-22 21:01:47 +0200

    add input field class and some fixes

--- snake.py
+++ snake.py
@@ -14,6 +14,7 @@ class Snake:
         self.body_img_left = pygame.image.load("resources/snake_body_left.png")
         self.body_img_right = pygame.image.load("resources/snake_body_right.png")
         self.body_img_up = pygame.image.load("resources/snake_body_up.png")
+        self.body_img_down = pygame.image.load("resources/snake_body_down.png")
 
         self.tail_img_down = pygame.image.load("resources/snake_tail_down.png")
         self.tail_img_up = pygame.image.load("resources/snake_tail_up.png")
@@ -40,36 +41,64 @@ class Snake:
         
 
     def draw(self):
-        if self.x[0] > self.x[1]:
-            self.parent_screen.blit(self.head_img_right, pygame.rect.Rect(self.x[0], self.y[0], 128, 128))
-            self.parent_screen.blit(self.tail_img_right, pygame.rect.Rect(self.x[self.length-1], self.y[self.length-1], 128, 128))
-        elif self.x[0] < self.x[1]:
-            self.parent_screen.blit(self.head_img_left, pygame.rect.Rect(self.x[0], self.y[0], 128, 128))
-            self.parent_screen.blit(self.tail_img_left, pygame.rect.Rect(self.x[self.length-1], self.y[self.length-1], 128, 128))
-        elif self.y[0] > self.y[1]:
-            self.parent_screen.blit(self.head_img_down, pygame.rect.Rect(self.x[0], self.y[0], 128, 128))
-            self.parent_screen.blit(self.tail_img_down, pygame.rect.Rect(self.x[self.length-1], self.y[self.length-1], 128, 128))
-        elif self.y[0] < self.y[1]:
-            self.parent_screen.blit(self.head_img_up, pygame.rect.Rect(self.x[0], self.y[0], 128, 128))
-            self.parent_screen.blit(self.tail_img_up, pygame.rect.Rect(self.x[self.length-1], self.y[self.length-1], 128, 128))
-        
+
+        self.update_head_img()
+        self.update_tail_img()
         for i in range(self.length):
-            if not i == 0 and not i == self.length-1:
-                if self.x[i] > self.x[i+1] and self.y[i] > self.y[i+1]:
-                    self.parent_screen.blit(self.curve_img_up_right, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))  
-                elif self.x[i] < self.x[i+1] and self.y[i] > self.y[i+1]:
-                    self.parent_screen.blit(self.curve_img_up_left, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))
-                elif self.x[i] < self.x[i+1] and self.y[i] < self.y[i+1]:
-                    self.parent_screen.blit(self.curve_img_down_left, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))
-                elif self.x[i] > self.x[i+1] and self.y[i] < self.y[i+1]:
-                    self.parent_screen.blit(self.curve_img_down_right, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))
-                elif self.x[i] > self.x[i+1]:
-                    self.parent_screen.blit(self.body_img_right, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))
-                elif self.x[i] < self.x[i+1]:
-                    self.parent_screen.blit(self.body_img_left, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))
-                else:
-                    self.parent_screen.blit(self.body_img_up, pygame.rect.Rect(self.x[i], self.y[i], 128, 128))
+            block_rect = pygame.rect.Rect(self.x[i], self.y[i], 128, 128)            
+            
+            if i == 0:
+                self.parent_screen.blit(self.head_img, block_rect)  
+                continue
+            if i == self.length - 1:
+                self.parent_screen.blit(self.tail_img, block_rect)  
+                continue
+            else:
+                previous_x = self.x[i-1] - self.x[i]
+                next_x = self.x[i+1] - self.x[i]
+                previous_y = self.y[i-1] - self.y[i]
+                next_y = self.y[i+1] - self.y[i]
+                
+                if previous_x == next_x:
+                    self.parent_screen.blit(self.body_img_up, block_rect)
+                    continue
+                elif previous_y == next_y:
+                    self.parent_screen.blit(self.body_img_right, block_rect)
+                    continue
+                elif previous_x == -BLOCK_SIZE and next_y == -BLOCK_SIZE or previous_y == -BLOCK_SIZE and next_x == -BLOCK_SIZE:
+                    self.parent_screen.blit(self.curve_img_up_left, block_rect)  
+                    continue
+                elif previous_x == BLOCK_SIZE and next_y == -BLOCK_SIZE or previous_y == -BLOCK_SIZE and next_x == BLOCK_SIZE:
+                    self.parent_screen.blit(self.curve_img_up_right, block_rect)
+                    continue
+                elif previous_x == -BLOCK_SIZE and next_y == BLOCK_SIZE or previous_y == BLOCK_SIZE and next_x == -BLOCK_SIZE:
+                    self.parent_screen.blit(self.curve_img_down_left, block_rect)
+                    continue
+                elif previous_x == BLOCK_SIZE and next_y == BLOCK_SIZE or previous_y == BLOCK_SIZE and next_x == BLOCK_SIZE:
+                    self.parent_screen.blit(self.curve_img_down_right, block_rect)
+                    continue
         
+
+    def update_head_img(self):
+        if self.x[0] > self.x[1]:
+            self.head_img = self.head_img_right  
+        elif self.x[0] < self.x[1]:
+            self.head_img = self.head_img_left
+        elif self.y[0] > self.y[1]:
+            self.head_img = self.head_img_down
+        elif self.y[0] < self.y[1]:
+            self.head_img = self.head_img_up
+
+    def update_tail_img(self):
+        if self.x[self.length-2] > self.x[self.length-1]:
+            self.tail_img = self.tail_img_right
+        elif self.x[self.length-2] < self.x[self.length-1]:
+              self.tail_img = self.tail_img_left
+        elif self.y[self.length-2] > self.y[self.length-1]:
+            self.tail_img = self.tail_img_down
+        elif self.y[self.length-2] < self.y[self.length-1]:
+            self.tail_img = self.tail_img_up
+
 
     def move_left(self):
         self.x[0] -= BLOCK_SIZE
--- view.py
+++ view.py
@@ -53,8 +53,6 @@ class Menu:
         self.game.running = False
         pygame.quit()
 
-
-
     def draw_menu(self):
         self.draw_button(self.game.screen, BLACK, [self.size[0]/2 - 75, 30, 150, 50], 2, BORDER, 'Beach Snake')
         self.draw_button(self.game.screen, BLACK, [self.size[0]/3 - 75, self.size[1] - 150, 150, 50], 2, BORDER, 'Start', self.start_game)
@@ -90,3 +88,47 @@ class Menu:
             screen.blit(col_2_name,[sizePos[0] + 60 + sizePos[3] / 3,sizePos[1] + topScorer_height + 30 + (i * 30)])
             screen.blit(col_3_score,[sizePos[0] + 90 + sizePos[3] / 3 * 2,sizePos[1] + topScorer_height + 30 + (i * 30)])
 
+
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = (199, 199, 199)
+        self.text = text
+        self.font = pygame.font.SysFont('arial', 80)
+        self.txt_surface = self.font.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = (245, 245, 245) if self.active else (199, 199, 199)
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = self.font.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
